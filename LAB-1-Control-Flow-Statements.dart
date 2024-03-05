b37d192d82eb78deee0464b974b0b19c562c73f5
OliyadM <101395529+OliyadM@users.noreply.github.com>
2024-03-05 16:08:18 +0300

    control flow statements exercise 1

---
+++ LAB-1-Control-Flow-Statements.dart
@@ -0,0 +1,29 @@
+void main() {
+  int date = 1;
+  //exercise 1
+  switch (date) {
+    case 1:
+      print('monday');
+      break;
+    case 2:
+      print('Tuesday');
+      break;
+    case 3:
+      print('wednesday');
+      break;
+    case 4:
+      print('thursday');
+      break;
+    case 5:
+      print('Friday');
+      break;
+    case 6:
+      print('saturday');
+      break;
+    case 7:
+      print('Sunday');
+      break;
+    default:
+      print('invalid date');
+  }
+}
