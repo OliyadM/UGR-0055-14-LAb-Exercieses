46c22e66a73289a56b1063140191fb8acb3ee176
OliyadM <101395529+OliyadM@users.noreply.github.com>
2024-03-05 16:56:18 +0300

    collections exercise 2

--- LAB-1-Collections.dart
+++ LAB-1-Collections.dart
@@ -1,5 +1,6 @@
 void main() {
-   // exercise 1 
+  //exercise 1
+
   maxx(lst) {
     int tempmax = lst[0];
     for (int index = 0; index < lst.length; ++index) {
@@ -10,5 +11,33 @@ void main() {
     return tempmax;
   }
 
+
   print(maxx([1, 2, 3, 4]));
+
+
+ //exercise 2
+
+ 
+  String courseCode = '01';
+  int classNumber = 7;
+  String courseName = 'Mobile Development';
+
+  print('Course Code: ' + courseCode);
+  print('Class Number: ' + classNumber.toString());
+  print('Course Name: ' + courseName);
+
+  Map<String, dynamic> studentInfo = {
+    'courseCode': courseCode,
+    'classNumber': classNumber,
+    'courseName': courseName,
+  };
+
+  print('\nStudent Information:');
+  printMap(studentInfo);
+}
+
+void printMap(Map<dynamic, dynamic> map) {
+  for (var key in map.keys) {
+    print(key.toString() + ': ' + map[key].toString());
+  }
 }
--- LAB-1-Functions.dart
+++ LAB-1-Functions.dart
@@ -3,12 +3,28 @@ void main() {
 
   double average(lst) {
     double sum = 0;
-    for ( double num in lst) {
+    for (double num in lst) {
       sum += num;
     }
     return sum / lst.length;
   }
 
+//exercise 2
+void printMap(Map<dynamic, dynamic> map) {
+  for (var key in map.keys) {
+    print(key, map[key]);
+  }
+}
+
+void main() {
+  Map<String, dynamic> myMap = {
+    'name': 'oliyad',
+    'age': 22,
+    'city': 'addis ababa',
+  };
+
+  printMap(myMap);
+}
 
 
 
